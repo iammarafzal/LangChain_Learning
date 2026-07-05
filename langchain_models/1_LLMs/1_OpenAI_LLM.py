ba33b83cb15ce494c7c8ba492cde44ad09d64975
Ammar Afzal <ammarafzal277@gmail.com>
2026-07-05 14:45:58 +0500

    Remove deprecated output parsers and prompt templates from langchain_output_parsers and langchain_prompts directories

--- langchain_models/1_LLMs/1_OpenAI_LLM.py
+++
@@ -1,8 +0,0 @@
-from langchain_openai import OpenAI
-from dotenv import load_dotenv
-
-load_dotenv()
-
-llm = OpenAI(model="")
-result = llm.invoke("")
-print(result)
--- langchain_structured_output/typedict_demo.py
+++
@@ -1,9 +0,0 @@
-from typing import TypedDict
-
-class Person(TypedDict):
-    name: str
-    age: int
-
-new_person: Person = {'name': "Ammar Afzal", 'age': 21}
-
-print(new_person['name'])
